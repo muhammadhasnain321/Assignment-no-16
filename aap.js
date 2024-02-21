96bdd4ec3ada55f36e355e7b5603310e75c50eb2
muhammadhasnain321 <princ.hasnain3@gmail.com>
2024-02-21 14:12:30 +0500

    complet my assignment

---
+++ aap.js
@@ -0,0 +1,241 @@
+// alert("Sir Faraz is my teacher");
+// document.write(25);
+// console.log("farooq is my lab atttendent");
+
+
+// alert(typeof "sir faraz");
+
+
+// var a = "muhammad"
+// var b = "hasnain"
+
+// console.log(a + " " + b)
+
+// document.write("Suleman is my classfellow");
+
+
+
+
+// chapter 1
+
+alert("<-------|||--CHAPTER NO 1--||------->")
+
+// // q 1
+alert("Wellcome to my page")
+
+// // q 2
+
+
+alert("Error! Please enter a valid password")
+
+
+// q 3
+alert("Wlcome to JS Land... \n Happy Coding")
+
+// q 4
+alert("Welcome to JS Land...")
+
+alert("Happy Coding! \n Prevent this page from creating additional dialogs.")
+
+// q 5
+alert("Hello... I can run JS through my web brower's console")
+
+
+
+// chapter 2
+
+alert("<-------|||--CHAPTER NO 2--||------->")
+
+
+// q 1
+
+var usernam = "jhon"
+
+
+// q2
+var mas = "Certified Mobile Application Development"
+var age = "15 years old"
+var myName = "Muhmmad Hasnain"
+
+// q3
+var message = "Hello World"
+
+
+alert(message)
+
+// q 4
+alert(myName)
+alert(age)
+alert(mas)
+
+
+// q5
+
+
+
+alert(" PIZZA \n PIZZ \n PIZ \n PI \n P")
+
+//  q6
+
+var email = "Hasnaingd@xyzcom"
+
+alert("My email address is "  + email )
+
+// q6
+var jawa = "A smarter way to learn JavaScript"
+
+alert("I am trying to learn from the Book " + jawa)
+
+// q7
+ document.write("Yah! I can write HTML content through JawaScript")
+
+//  q8
+alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”")
+
+
+// Chapter 3
+
+
+alert("<-------|||--CHAPTER NO 3--||------->")
+
+
+// q1
+ 
+var age = 15
+
+alert("I am" + age + " years old")
+
+// q 2
+
+var n = 14
+
+alert("You have visited this site " + n + " times")
+
+
+// q3
+
+ var birthyear = 1994
+
+ document.write("My birth year is " + birthyear);
+
+ document.write("<br>")
+
+ document.write("Data type of my ddeclared variable is number")
+
+ document.write("<br>")
+
+//  q4
+
+var visitername = "<b>John Doe </b>"
+var ptittle = "<b> T-shirt</b>"
+var quantity = "<b>5</b>"
+
+
+document.write(visitername + "ordered " + quantity + ptittle +"(s) on XYZ Clothing Store") 
+
+
+// chapter 4
+
+alert("<-------|||--CHAPTER NO 4--||------->")
+
+// chapter 1
+
+
+//  q1
+
+var firstName = "MUHAMMAD"
+var lastName = "HASNAIN"
+var email = "MUhammadhasnain321@gmail.com"
+var phoneNumber = "0313-0000000"
+var password =  270010312
+
+
+alert("First Name: " + firstName);
+alert("Last Name: " + lastName);
+alert("Email: " + email);
+alert("Phone Number: " + phoneNumber);
+alert("Password: " + password);
+
+// q2
+
+alert("You're learning JavaScript!");
+
+// q3
+
+
+alert("Assalam-o-Alaikum! \n  welcome to my page");
+
+
+// chapter 2
+
+// q1
+
+
+ var mySchoolName = "North Star Public School"
+ document.write("my school name" + mySchoolName)
+
+//  q 2
+
+var Suleman
+
+Suleman = "My friend"
+
+document.write("<br>");
+document.write("my freiend name: " + Suleman);
+
+
+// q 3
+var teamName = "Muslim Khatri";
+
+document.write("<br>");
+
+alert("our team name is " + teamName);
+
+// q 4
+
+var bestMan = "Charlie";
+
+bestMan = "Suleman";
+document.write("<br>");
+document.write("our best man is " + bestMan);
+
+
+
+//  chapter 3
+
+// q 1
+ var caseQty = "144";
+
+document.write("<br>");
+document.write("this prodect prive is " + caseQty + " ");
+ 
+
+//  q 2
+var  num ="9";
+document.write("<br>");
+
+document.write("our team number is " + num);
+
+// q3
+var sum = 25+9;
+document.write("<br>");
+
+document.write("our total sum number is" +" " +sum);
+
+
+
+// q 4
+
+var merchTotal = 100;
+var shippingCharges = 10;
+var orderTotal = merchTotal + shippingCharges
+
+console.log(orderTotal);
+
+// q 5
+var num =19;
+num = num + 6;
+
+document.write("<br>");
+
+document.write(num)
